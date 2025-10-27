dbfcc6b25f60df534bfbaf43a74f81fd53ee3562
Daan Vansteenhuyse <daan.vansteenhuyse@kuleuven.be>
2025-10-27 13:58:19 +0100

    Rewrote two queries to reflect changes in summary object

--- sql/2025/security/hsts_attributes.sql
+++ sql/2025/security/hsts_attributes.sql
@@ -3,8 +3,8 @@
 # Question: How many websites use HSTS includeSubDomains and preload?
 SELECT
   client,
-  COUNT(0) AS total_requests,
-  COUNTIF(hsts_header_val IS NOT NULL) AS total_hsts_headers,
+  COUNT(0) AS total_requests_with_hsts_header,
+  COUNTIF(hsts_header_val IS NOT NULL) AS total_non_null_hsts_headers,
   SAFE_DIVIDE(COUNTIF(hsts_header_val IS NOT NULL), COUNT(0)) AS pct_hsts_requests,
   SAFE_DIVIDE(COUNTIF(REGEXP_CONTAINS(hsts_header_val, r'(?i)max-age\s*=\s*\d+') AND NOT REGEXP_CONTAINS(CONCAT(hsts_header_val, ' '), r'(?i)max-age\s*=\s*0\W')), COUNTIF(hsts_header_val IS NOT NULL)) AS pct_valid_max_age,
   SAFE_DIVIDE(COUNTIF(REGEXP_CONTAINS(CONCAT(hsts_header_val, ' '), r'(?i)max-age\s*=\s*0\W')), COUNTIF(hsts_header_val IS NOT NULL)) AS pct_zero_max_age,
@@ -13,13 +13,14 @@ SELECT
 FROM (
   SELECT
     client,
-    REGEXP_EXTRACT(STRING(summary.respOtherHeaders), r'(?i)strict-transport-security =([^,]+)') AS hsts_header_val
+    response_headers.value AS hsts_header_val
   FROM
-    `httparchive.crawl.requests`
+    `httparchive.crawl.requests`,
+    UNNEST(response_headers) AS response_headers
   WHERE
     date = '2025-07-01' AND
     is_root_page AND
-    is_main_document
+    LOWER(response_headers.name) = 'strict-transport-security'
 )
 GROUP BY
   client
